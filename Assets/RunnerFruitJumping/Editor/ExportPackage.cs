890c15f06753e8058f80f801daa68edca2b5a788
hanxu <11521142@qq.com>
2018-09-11 11:19:59 +0800

    Initial commit

---
+++ Assets/RunnerFruitJumping/Editor/ExportPackage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class ExportPackage : Editor {
+
+    [MenuItem("ExportProject/Export")]
+    public static void Export()
+    {
+        string[] projectContent = AssetDatabase.GetAllAssetPaths();
+        AssetDatabase.ExportPackage(projectContent, "UltimateTemplate.unitypackage", ExportPackageOptions.Recurse | ExportPackageOptions.IncludeLibraryAssets);
+        Debug.Log("Project Exported");
+    }
+}
---
+++ Assets/RunnerFruitJumping/Scripts/UiManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+	public DataGame dataGame;
+	public GameManager gameManager;
+	public PlayerControl playControl;
+	public Text scorePlay;
+	public Text coinPlay;
+	public Text scoreOver;
+	public Text bestOver;
+	public Animator animaUI;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		animaUI = GetComponent<Animator> ();
+	}
+		
+	void Start ()
+	{
+		animaUI.SetBool ("start", true);
+		SetCoin ();
+	}
+
+	public void ButtonPlay ()
+	{
+		animaUI.SetBool ("start", false);
+		animaUI.SetBool ("play", true);
+	}
+
+	public void GameOver ()
+	{
+		animaUI.SetBool ("play", false);
+		animaUI.SetBool ("gameOver", true);
+		if (!PlayerPrefs.HasKey ("best") || PlayerPrefs.GetInt ("best") < dataGame.score) {
+			PlayerPrefs.SetInt ("best", dataGame.score);
+		}
+		scoreOver.text = "Score : " + dataGame.score.ToString ();
+		bestOver.text = "Best : " + PlayerPrefs.GetInt ("best").ToString ();
+		//UltimateSevice.Instances.SumitService (dataGame.score);
+//		if (Random.Range (0, 3) % 3 == 1) {
+//			UltimateAds.Instances.ShowFull ();
+//		}
+	}
+
+	public void ButtonRestart ()
+	{
+		animaUI.SetBool ("gameOver", false);
+		animaUI.SetBool ("play", true);
+	}
+
+	public void ButtoShop ()
+	{
+		animaUI.SetBool ("shop", true);
+	}
+
+	public void BackShop ()
+	{
+		animaUI.SetBool ("shop", false);
+	}
+
+	public void SetScore ()
+	{
+		scorePlay.text = dataGame.score.ToString ();
+	}
+
+	public void SetCoin ()
+	{
+		coinPlay.text = dataGame.coin.ToString ();
+	}
+
+	public void Pingcoin ()
+	{
+		animaUI.SetTrigger ("ping");
+	}
+}
